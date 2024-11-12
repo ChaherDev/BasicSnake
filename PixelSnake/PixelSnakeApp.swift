f284b68c048914d348118825d8d24183b5a08b69
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-11-12 15:01:26 +0100

    Initial Commit

---
+++ PixelSnake/PixelSnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelSnakeApp.swift
+//  PixelSnake
+//
+//  Created by Chaher Machhour on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PixelSnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
